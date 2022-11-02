adfea14311949f4c4b67b4853cbfdbdc27baaceb
David Ludemann <dludemann@aspiralabs.co>
2022-11-02 14:40:57 -0400

    initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -3,35 +3,41 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
+import { babel } from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import cleaner from "rollup-plugin-cleaner";
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
+        dir: "dist",
         format: "esm",
+        preserveModules: true,
         sourcemap: true,
       },
     ],
     plugins: [
+      cleaner({ targets: "dist" }),
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      babel({
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        exclude: /node_modules/,
+        presets: [
+          ["@babel/preset-env", { targets: "defaults" }],
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        outDir: "dist",
+        declarationDir: "dist",
+      }),
       postcss(),
     ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.(css|less|scss)$/],
   },
 ];
