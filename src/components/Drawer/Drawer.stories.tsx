46ab075b4143e50ecdd10965c133b440362694a3
dludemann <dludemann@aspiralabs.co>
2022-12-01 13:15:08 -0500

    Drawer Updates

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import Button from '../Button';
+import Form from '../Form';
 import Drawer from './Drawer';
+import Input from '../Input';
 
 export default {
-    title: 'Drawer',
+    title: 'Drawer'
 };
 
 export const BasicDrawer = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [drawerOpenTwo, setDrawerOpenTwo] = useState(false);
 
+    const handleSubmit = (values: any) => {
+        console.log(values);
+    };
+
     return (
         <div className="flex gap-4">
-            <Button onClick={() => setDrawerOpen(true)}>Open Left</Button>
             <Button onClick={() => setDrawerOpenTwo(true)}>Open Right</Button>
 
-            <Drawer open={drawerOpen} width={400} location="left">
-                <Button onClick={() => setDrawerOpen(false)}>Close</Button>
-            </Drawer>
-
-            <Drawer open={drawerOpenTwo} width={400} location="right">
-                <Button onClick={() => setDrawerOpenTwo(false)}>Close</Button>
+            <Drawer open={drawerOpenTwo} width={400} location="right" onClose={() => setDrawerOpenTwo(false)}>
+                <section className="p-8">
+                    <Form onSubmit={handleSubmit}>
+                        <Input name="firstName" label="First Name" />
+                        <Input name="lastName" label="Last Name" />
+                    </Form>
+                </section>
             </Drawer>
         </div>
     );
--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,26 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
+import { onClickOutside } from '../../hooks';
 
 interface DrawerProps {
     location?: 'right' | 'left';
     open: boolean;
     children?: any;
     width?: number;
+    onClose?: any;
 }
 
 const Drawer = (props: DrawerProps) => {
-    const { width = 300, location = 'right' } = props;
+    const { width = 300, location = 'right', onClose } = props;
     const controls = useAnimation();
+    const drawerRef = useRef<any>(null);
 
     const drawerAnimationStyles = {
         active: { x: location === 'right' ? `calc(100vw - ${width}px)` : 0 },
-        inactive: { x: location === 'right' ? '100vw' : -width },
+        inactive: { x: location === 'right' ? '100vw' : -width }
     };
 
     const backdropAnimationStyles = {
         active: { background: 'rgba(0,0,0,0.25)' },
-        inactive: { background: 'rgba(0,0,0,0)' },
+        inactive: { background: 'rgba(0,0,0,0)' }
     };
+
+    onClickOutside(drawerRef, () => {
+        onClose && onClose();
+    });
 
     // =========================================================================
     // EFFECTS
@@ -36,6 +43,7 @@ const Drawer = (props: DrawerProps) => {
             variants={backdropAnimationStyles}
             initial="inactive"
             transition={{ type: 'spring', damping: 10, mass: 0.2, stiffness: 100 }}
+            style={{ zIndex: 100 }}
         >
             <motion.div
                 className="h-screen bg-white pointer-events-auto"
@@ -44,6 +52,7 @@ const Drawer = (props: DrawerProps) => {
                 variants={drawerAnimationStyles}
                 initial="inactive"
                 transition={{ type: 'spring', damping: 10, mass: 0.2, stiffness: 100 }}
+                ref={drawerRef}
             >
                 {props.children}
             </motion.div>
